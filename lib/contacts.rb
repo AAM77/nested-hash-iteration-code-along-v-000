6c20939206621f9cc6e76940a120702ce5b87ed0
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-24 20:19:46 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         attribute.reject{|flavor| flavor == "strawberry"}
-
+      end
     end
   end
 end
